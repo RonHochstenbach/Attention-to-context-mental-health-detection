14a946177332416449cd36a566cb1ea9ab3e0e18
RonHochstenbach <hochstenbach.ron@gmail.com>
2023-04-13 15:14:01 +0200

    Update data_loader.py

--- Code/data_loader.py
+++ Code/data_loader.py
@@ -1,4 +1,4 @@
-from resource_loader import load_vocabulary, load_NRC
+from resource_loader import load_vocabulary, load_NRC, load_LIWC
 
 def load_erisk_data(writings_df, hyperparams_features, by_subset=True,
                     pronouns = ["i", "me", "my", "mine", "myself"],
@@ -8,9 +8,62 @@ def load_erisk_data(writings_df, hyperparams_features, by_subset=True,
                     user_level=True, labelcol='label', label_index=None,
                    logger=None):
 
+    #importing vocabulary, lexicon and liwc
     vocabulary = load_vocabulary(hyperparams_features['vocabulary_path'])
     vocab_size = hyperparams_features(['max_features'])
 
     emotion_lexicon = load_NRC(hyperparams_features['nrc_lexicon_path'])
     emotions = list(emotion_lexicon.keys())
+
+    liwc_dict = load_LIWC(hyperparams_features['liwc_path'])
+    liwc_categories = set(liwc_dict.keys())
+
+    #creating train, validation and test sets
+    training_subjects = list(set(writings_df[writings_df['subset']=='train'].subject))
+    test_subjects = list(set(writings_df[writings_df['subset'] == 'test'].subject))
+
+    training_subjects = sorted(training_subjects) #ensure reproducability
+    valid_subjects_size = int(len(training_subjects)*valid_prop)
+    validation_subjects = training_subjects[:valid_subjects_size]
+    training_subjects = training_subjects[valid_subjects_size:]
+
+    categories = [c for c in liwc_categories if c in writings_df.columns]
+
+    subjects_split = {'train': training_subjects,
+                      'validation': validation_subjects,
+                      'test': test_subjects}
+
+    user_level_texts = {}
+    for row in writings_df.sort_values(by='date').itertuples():
+        words = []
+        raw_text = ""
+
+        if hasattr(row, 'tokenized_title'):
+            if row.tokenized_title:
+                words.extend(row.tokenized_title)
+                raw_text += row.title
+
+        if hasattr(row, 'tokenized_text'):
+            if row.tokenized_text:
+                words.extend(row.tokenized_text)
+                raw_text += row.text
+
+        if labelcol == 'label':
+            label = row.label
+
+        liwc_categs = [getattr(row, categ) for categ in categories]
+
+        if row.subject not in user_level_texts.keys():
+            user_level_texts[row.subject] = {}
+            user_level_texts[row.subject]['texts'] = [words]
+            user_level_texts[row.subject]['label'] = label
+            user_level_texts[row.subject]['liwc'] = [liwc_categs]
+            user_level_texts[row.subject]['raw'] = [raw_text]
+        else:
+            user_level_texts[row.subject]['texts'].append(words)
+            user_level_texts[row.subject]['liwc'].append(liwc_categs)
+            user_level_texts[row.subject]['raw'].append(raw_text)
+
+    return user_level_texts, subjects_split, vocabulary
+
 
